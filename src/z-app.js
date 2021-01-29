0dc05a72ae495ec22c033b396cf7b23e56c0ba79
alexboldsolutions <aleksandar@boldsolutions.hr>
2021-01-29 16:38:49 +0100

    eslint prettier

--- src/z-app.js
+++ src/z-app.js
@@ -1,20 +1,24 @@
-import { LitElement, html, css } from 'lit-element';
-import './components/circle-spinner'
+import { LitElement, html } from 'lit-element';
+import './components/circle-spinner';
 
 class ZApp extends LitElement {
-	// static get styles() {
-	// 	return css`...`
-	// }
+  // static get styles() {
+  // 	return css`...`
+  // }
 
-	// static get properties() {
-	// 	return { prop: propType };
-	// }
-
-	render() {
-		return html`
-			<h3>App 1</h3>
-			<circle-spinner></circle-spinner>
-		`
-	}
+  // static get properties() {
+  // 	return { prop: propType };
+  // }
+  showAlert(text) {
+    alert(text);
+  }
+  render() {
+    return html`
+      <h3>App 1</h3>
+      <circle-spinner
+        @click=${() => this.showAlert('Clicked me')}
+      ></circle-spinner>
+    `;
+  }
 }
 customElements.define('z-app', ZApp);
